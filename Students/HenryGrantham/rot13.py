bc2e3cfaa06c241846451f3f94cf8553616b4350
Henry Grantham <hgrantham@gmail.com>
2014-12-06 20:05:29 -0800

    Small changes added the rot13 translation function and None case, but stillneed to create a translation table.

--- Students/HenryGrantham/rot13.py
+++ Students/HenryGrantham/rot13.py
@@ -5,58 +5,36 @@
 # This module.
 
 
-def rot_letter(text):
-    """Given a letter, return a letter 13 letters after it.
-
-    text is the letter to rotate.
-    if text is not a letter or has nothing in it, then return None.
-    if the sting is longer than 1 character than only use the first
-        character.
-    if text is a space, then return a space.
-    if text is a capital letter than returns a capital letter
-        13 letters after it, wrapping around after z.
-    if text is a small case letter than  returns a small case letter
-        13 letters after it, wrapping around after z.
-    """
-    if text is None:
-        print(u"The string is None.")
-        return None
-    if text == "":
-        print(u"The string has nothing in it.")
-        return None
-    text = text[0]
-    if text.isspace():
-        return text
-    elif text.isalpha():
-        print text
-        return text
-    else:
-        print(u"The string does not contain a letter.")
-        return None
-
-
 def rot13(text):
     """Given a string, return a ROT13 encoded string.
 
     text is the string to encode.
-    if text does not concatain all letters then return None.
+
+    if text is None return None
+    if string is empty, return an empty string.
+
+    Everything that is not a letter in text will not be changed.
+    All letters in this string will be shifted 13 letters forward,
+        wrapping around after z with capital vs. small cased letters
+        preserved.
     """
-    if text.isalpha():
-        pass
-    else:
-        print(u"The string does not contain all letters.")
+    if text is None:
         return None
 
+    table = None
+    return text.translate(table)
+
+
 if __name__ == '__main__':
-    # Testing rot13_letter
+    # Testing ROT13
     # check for case where string is empty or contains a number
     #   or symbol or space or is None
 
-    assert rot_letter(None) is None
-    assert rot_letter("") is None
-    assert rot_letter("0") is None
-    assert rot_letter("^") is None
-    assert rot_letter("i") == "i"
+    assert rot13(None) is None
+    assert rot13("") == ""
+    assert rot13("01234") == "01234"
+    assert rot13("^$&*") == "^$&*"
+    assert rot13("This is a test") == "This is a test"
 
-    print(rot_letter("i"))
+    print(rot13("This is a test"))
 
