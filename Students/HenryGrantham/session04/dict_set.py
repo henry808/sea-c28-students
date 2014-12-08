f024b4299d6a3bcac1301399f7b8bb8f14cb7930
Henry Grantham <hgrantham@gmail.com>
2014-12-07 21:35:02 -0800

    dict_set.py is done and works as intended
    Everything works fine

--- Students/HenryGrantham/session04/dict_set.py
+++ Students/HenryGrantham/session04/dict_set.py
@@ -2,7 +2,7 @@
 
 # dict_set.py
 #
-# Create a dictionary and manipulate it.
+# Create dictionaries and sets and manipulate them.
 
 
 if __name__ == '__main__':
@@ -54,12 +54,41 @@ if __name__ == '__main__':
 
     print(inthexdict.items())
 
-    # Make a dictionary with 0..15 as keys and # of a's as values
-    for i in range(len(hexlist)):
-        hexlist[i] = 'a' * i
-
-    inthexdict = dict(zip(intlist, hexlist))
+    # Using same dictionary, change values to key # of a's
+    for k in inthexdict.keys():
+        inthexdict[k] = 'a' * k
 
     print(inthexdict.items())
 
+    # Sets s2, s3 and s4 contain #'s from
+    #     0 through 20, divisible 2, 3 and 4.
+    s2 = set()
+    s3 = set()
+    s4 = set()
+    for i in range(21):
+        if i / 2 == float(i) / 2:
+            s2.add(i)
+        if i / 3 == float(i) / 3:
+            s3.add(i)
+        if i / 4 == float(i) / 4:
+            s4.add(i)
+    # Display sets
+    print(s2)
+    print(s3)
+    print(s4)
 
+    # Display if s3 is a subset of s2
+    print s3.issubset(s2)
+    # Display if s4 is a subset of s2
+    print s4.issubset(s2)
+
+    # Create a set of letters of Python
+    setofstrings = set("Python")
+    setofstrings.add('i')
+    print(setofstrings)
+
+    # Create a frozenset of letters in 'marathon'
+    fsetofstrings = set('marathon')
+    print(fsetofstrings)
+    print setofstrings.union(fsetofstrings)
+    print setofstrings.intersection(fsetofstrings)
