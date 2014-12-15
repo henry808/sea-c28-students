6bd6c60d23d6ba854bb611ea61dbd4d495f2cb21
Henry Grantham <hgrantham@gmail.com>
2014-12-14 20:20:46 -0800

    lambda_magic.py totally completed, really

--- Students/HenryGrantham/session06/lambda_magic.py
+++ Students/HenryGrantham/session06/lambda_magic.py
@@ -2,27 +2,25 @@
 
 # lambda_magic.py
 #
-# Create dictionaries and sets and manipulate them.
+# Contains one function that returns a list of functions.
+# Also contains code that tests function.
 from __future__ import print_function
 
 
 def function_builder(number):
     """Return a list of functions that add 0,1,2,... to a number.
 
-
+    The nth function in the list adds n to the input.
     """
-    list_functions = [lambda x, z=i: x+z for i in range(number)]
-
-    # list_functions = []
-    # for i in range(number):
-    #     list_functions.append(lambda x, z=i: x + z)
-    return list_functions
+    return [lambda x, z=i: x+z for i in range(number)]
 
 if __name__ == '__main__':
 
     the_list = function_builder(4)
 
     print(the_list)
+
+    print("The list contains %s functions." % len(the_list))
 
     print(the_list[0](2))
     print("\n")
@@ -32,3 +30,5 @@ if __name__ == '__main__':
 
     for f in the_list:
         print(f(5))
+
+    print("Everything checks out.")
