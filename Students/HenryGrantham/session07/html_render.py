1b83cccf149bf2270e3bbd7a12ee9b44b82a01b1
Henry Grantham <hgrantham@gmail.com>
2014-12-17 00:55:35 -0800

    html_render.py start almost working, just need to add
    just need to add indents

--- Students/HenryGrantham/session07/html_render.py
+++ Students/HenryGrantham/session07/html_render.py
@@ -7,5 +7,17 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+    def __init__(self):
+        self.content = ""
+        self.beg_instr = "<>\n"
+        self.end_str = "\n</>\n"
+
+    def append(self, text):
+        self.content = "{}\n{}".format(self.content, text)
+
+    def render(self, file):
+        file.write("{}{}{}".format(self.beg_instr, self.content, self.end_str))
