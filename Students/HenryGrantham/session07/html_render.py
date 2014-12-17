d20fc69cc0496c6ec85907648cdcff4dbcdc81fb
Henry Grantham <hgrantham@gmail.com>
2014-12-17 02:04:36 -0800

    html_render.py step 1
    Still on step 1. Fixed ind now that I understand what it means.

--- Students/HenryGrantham/session07/html_render.py
+++ Students/HenryGrantham/session07/html_render.py
@@ -10,14 +10,23 @@ Python class example.
 
 
 class Element(object):
+    """An html element.
+
+    An html element for rendering.
+    """
+    indent = "   "
 
     def __init__(self):
         self.content = ""
-        self.beg_instr = "<>\n"
-        self.end_str = "\n</>\n"
+        self.beg_tag = "<>\n"
+        self.end_tag = "</>\n"
 
     def append(self, text):
         self.content = "{}\n{}".format(self.content, text)
 
-    def render(self, file):
-        file.write("{}{}{}".format(self.beg_instr, self.content, self.end_str))
+    def render(self, file_out, ind="    "):
+        self.content = self.content.replace("\n", "\n" + self.indent + ind)
+        file_out.write("{id}{}{}\n{id}{}".format(self.beg_tag,
+                                                 self.content,
+                                                 self.end_tag,
+                                                 id=ind))
