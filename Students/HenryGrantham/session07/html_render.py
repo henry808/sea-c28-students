dfc92e81fb8da22243e41e93c5d5a9631d1f1e49
Henry Grantham <hgrantham@gmail.com>
2014-12-17 03:48:14 -0800

    html_render.py
    step two done

--- Students/HenryGrantham/session07/html_render.py
+++ Students/HenryGrantham/session07/html_render.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Python class example.
+Html package
 
 """
 
@@ -14,19 +14,50 @@ class Element(object):
 
     An html element for rendering.
     """
-    indent = "   "
+    indent = "    "
+    beg_tag = "<>\n"
+    end_tag = "</>\n"
 
-    def __init__(self):
-        self.content = ""
-        self.beg_tag = "<>\n"
-        self.end_tag = "</>\n"
+    def __init__(self, element=None):
+        self.content = []
+        if not (element is None):
+            self.content.append(element)
 
-    def append(self, text):
-        self.content = "{}\n{}".format(self.content, text)
+    def append(self, element):
+        self.content.append(element)
 
-    def render(self, file_out, ind="    "):
-        self.content = self.content.replace("\n", "\n" + self.indent + ind)
-        file_out.write("{id}{}{}\n{id}{}".format(self.beg_tag,
-                                                 self.content,
-                                                 self.end_tag,
-                                                 id=ind))
+    def render(self, file_out, ind=""):
+        file_out.write("{}{}".format(ind, self.beg_tag))
+        if type(self) == Element:
+            for text in self.content:
+                file_out.write("{}{}{}".format(ind, self.indent, text))
+                file_out.write("\n")
+        elif type(self) == P:
+            # this only works for paragraphs with one line.
+            file_out.write("{}{}{}".format(ind, self.indent, self.content[0]))
+            file_out.write("\n")
+        else:
+            print self.content
+            for content in self.content:
+                content.render(file_out, self.indent + ind)
+        file_out.write("{}{}".format(ind, self.end_tag))
+
+
+class Body(Element):
+    """A body html element
+    """
+    beg_tag = "<body>\n"
+    end_tag = "</body>\n"
+
+
+class P(Element):
+    """A p html element
+    """
+    beg_tag = "<p>\n"
+    end_tag = "</p>\n"
+
+class Html(Element):
+    """A p html element
+    """
+    beg_tag = "<html>\n"
+    end_tag = "</html>\n"
