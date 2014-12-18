f62efb9dd47ebf66491051e2d1da8d628b9caba3
Henry Grantham <hgrantham@gmail.com>
2014-12-17 18:53:56 -0800

    commiting so that I can ckekout master

--- Students/HenryGrantham/session07/circle.py
+++ Students/HenryGrantham/session07/circle.py
@@ -7,6 +7,16 @@ import math
 
 
 class Circle(object):
-    def __init__(self, radius):
-            self.radius = radius
-        
+    beg_instr = "<>"
+    end_str = "</>"
+    string = """<>
+    Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text
+    And here is another piece of text -- you should be able to add any number
+</>"""
+
+    def __init__(self):
+        pass
+
+    def render(self, text):
+
+        return "".format(beg_instr, text, end_str)
