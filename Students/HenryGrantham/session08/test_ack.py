f241dba4b073c377bb2fe49e813fdee6b6d5111c
Henry Grantham <hgrantham@gmail.com>
2014-12-28 14:03:40 -0800

    test_ack.py changed four tests into one nested for loop test

--- Students/HenryGrantham/session08/test_ack.py
+++ Students/HenryGrantham/session08/test_ack.py
@@ -27,30 +27,11 @@ def test_inputstr():
 def test_inputstr0i():
     """Test with 0 and i where i is 0 to 4
     """
-    mylist = [1, 2, 3, 4, 5]
+    mylist = [[1, 2, 3, 4, 5],
+              [2, 3, 4, 5, 6],
+              [3, 5, 7, 9, 11],
+              [5, 13, 29, 61, 125]]
+
     for i in range(4):
-        assert ack.ack(0, i) == mylist[i]
-
-
-def test_inputstr1i():
-    """Test with 1 and i where i is 0 to 4
-    """
-    mylist = [2, 3, 4, 5, 6]
-    for i in range(4):
-        assert ack.ack(1, i) == mylist[i]
-
-
-def test_inputstr2i():
-    """Test with 2 and i where i is 0 to 4
-    """
-    mylist = [3, 5, 7, 9, 11]
-    for i in range(4):
-        assert ack.ack(2, i) == mylist[i]
-
-
-def test_inputstr3i():
-    """Test with 3 and i where i is 0 to 4
-    """
-    mylist = [5, 13, 29, 61, 125]
-    for i in range(4):
-        assert ack.ack(3, i) == mylist[i]
+        for j in range(5):
+            assert ack.ack(i, j) == mylist[i][j]
